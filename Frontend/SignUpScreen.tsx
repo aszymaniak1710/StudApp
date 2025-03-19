a651db99f8f3e9822a3808aead526a0dfaa1345f
aszymaniak1710 <aszymaniak1710@gmail.com>
2025-03-19 23:53:17 +0100

    Zdjęte security, debug frontendu-frontend

---
+++ Frontend/SignUpScreen.tsx
@@ -0,0 +1,109 @@
+// SignUpScreen.tsx
+import React, { useState } from 'react';
+import { View, TextInput, Button, Text, Alert, StyleSheet } from 'react-native';
+import api from "./api";
+
+const SignUpScreen = ({ navigation }: any) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSignUp = async () => {
+    setLoading(true);
+
+    if (password !== password2) {
+      Alert.alert("Błąd", "Hasła nie są takie same!");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await api.post('http://192.168.1.26:8080/register', {
+        username: username,
+        password: password,
+      });
+
+      const jsonResponse = JSON.stringify(response.data);
+
+      if (response.data && response.data.success) {
+        Alert.alert('Rejestracja udana!', jsonResponse);
+        navigation.navigate('SignIn');
+      } else {
+        Alert.alert(jsonResponse);
+      }
+    } catch (error: any) {
+      console.error('Błąd rejestracji:', error.message);
+      Alert.alert('Błąd rejestracji', 'Wystąpił problem z połączeniem');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Rejestracja</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nazwa użytkownika"
+        value={username}
+        onChangeText={setUsername}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Hasło"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+
+      <TextInput
+          style={styles.input}
+          placeholder="Powtórz hasło"
+          value={password2}
+          onChangeText={setPassword2}
+          secureTextEntry
+      />
+
+      <Button title="Zarejestruj się" onPress={handleSignUp} disabled={loading} />
+      <Text style={styles.footer}>
+        Masz już konto? <Text style={styles.link} onPress={() => navigation.navigate('LoginScreen')}>Zaloguj się</Text>
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  header: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
+  },
+  footer: {
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  link: {
+    color: '#007bff',
+  },
+});
+
+
+export default SignUpScreen;
